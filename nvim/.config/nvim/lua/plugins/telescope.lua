8223a38349f5be4baed56eea0b1ff0e0ce10775e
Dan Jenkins <deiussum@gmail.com>
2025-12-20 04:30:55 +0000

    Added guard around telescope nvim plugin

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,8 @@
-return {
-    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-}
+if vim.fn.has("nvim-0.10.4") == 1 then
+    return {
+        'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+          dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+end
+
+return {}
